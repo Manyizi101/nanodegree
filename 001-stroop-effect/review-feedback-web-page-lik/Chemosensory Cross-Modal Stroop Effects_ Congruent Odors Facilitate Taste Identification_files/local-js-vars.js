98a020da82d7201866b8935e2f2d530c376ff356
veiczeljko <veiczeljko@gmail.com>
2015-10-01 09:26:54 +0200

    beginning of the month backup

---
+++ 001-stroop-effect/review-feedback-web-page-lik/Chemosensory Cross-Modal Stroop Effects_ Congruent Odors Facilitate Taste Identification_files/local-js-vars.js
@@ -0,0 +1,52 @@
+
+/* 
+################################################################## 
+## 
+##   THIS IS A GENERATED FILE. 
+## 
+##   ANY CHANGES YOU MAKE DIRECTLY TO THIS FILE WILL BE OVERWRITTEN 
+##   AND LOST THE NEXT TIME THIS FILE IS GENERATED. 
+## 
+##  (This file last generated: Sep 23, 2015, 12:37 P.M.) 
+## 
+################################################################## 
+ */
+
+/* ##### BELOW added by: file:/highwire/webapps/chemse/resources/publisher/build/local-js-vars.base.xml ##### */
+
+
+
+var gJournalVars = {};
+
+
+
+var gJournalVars = {  
+  jnlID: 'chemse',
+   copy: "Oxford University Press",
+   issn: '0379-864X',
+  eissn: '1464-3553',
+  oupcode: 'chemse',
+  urlPrefix: 'chemse'
+};
+ 
+
+
+
+
+
+    var manuscript = false;
+
+
+
+ 
+
+
+
+	var CollapsibleTocs = false;
+
+
+
+
+
+
+/* ##### END OF ADDED BLOCK ##### */
