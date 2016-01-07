4b7e805d7d4dcb556c74476def4fa4368813cdc7
veich <veiczeljko@gmail.com>
2016-01-07 22:48:02 +0100

    DAND - P3 - added code that explains some general info about the data and fixes some street names etc.

--- 003-open-street-map/dev/processing.py
+++ 003-open-street-map/dev/processing.py
@@ -1,7 +1,7 @@
 """
 Your task in this exercise has two steps:
 
-- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix
+- audit the OSMFILE and change the variable 'abbreviation_mapping' to reflect the changes needed to fix
     the unexpected street types to the appropriate ones in the expected list.
     You have to add mappings only for the actual problems you find in this OSMFILE,
     not a generalized solution, since that may and will depend on the particular area you are auditing.
@@ -10,91 +10,91 @@ Your task in this exercise has two steps:
     We have provided a simple test so that you see what exactly is expected
 """
 import xml.etree.cElementTree as ET
-from collections import defaultdict
 import re
 import pprint
 
+###############################################################################################################
+
 OSMFILE = "data/zagreb_croatia.osm"
-street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+expected_street_types = ["ulica", "prolaz", "trg", "cesta", "avenija", "park", "put",
+                         "stube", "prilaz", "aleja", "perivoj", "zavoj", "ugao", "odvojak"]
+
+abbreviation_mapping = {"Ul.": "Ulica",
+                        "Av.": "Avenija"}
 
 
-#expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road",
-#            "Trail", "Parkway", "Commons"]
+###############################################################################################################
 
-expected = ["ulica", "prolaz", "trg", "cesta", "avenija", "park", "put", "stube", "prilaz", "aleja", "perivoj", "zavoj", "ugao", "odvojak"]
+def street_types_from_file(osmfile):
+    osm_file = open(osmfile, "r")
 
-# UPDATE THIS VARIABLE
-mapping = { "Ul.": "Ulica",
-            "Av.":"Avenija"
-            }
+    street_types_count = {"ulica": 0, "prolaz": 0, "trg": 0, "cesta": 0, "avenija": 0, "park": 0, "put": 0, "stube": 0,
+                          "prilaz": 0, "aleja": 0, "perivoj": 0, "zavoj": 0, "ugao": 0, "odvojak": 0, "error": 0}
 
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    sum_streets_by_type(street_types_count, tag.attrib['v'])
 
-def audit_street_type(street_types, street_name):
-    global mapping
-    found = False
-    for name in expected:
-        idx = street_name.lower().find(name)
-        if idx >= 0:
-            found = True
-    if street_name.lower().strip().find(' ') < 0:
-        found = True
-        #print street_name
-        try:
-            print int(street_name)
-        except:
-            pass
-    if found == False and (street_name not in street_types):
-        street_types.add(street_name)
-        for key in mapping:
-            if street_name.lower().find(key.lower()) >= 0:
-                print street_name
+    print '\nStreet type count:'
+    return street_types_count
 
 
+###############################################################################################################
 
 def is_street_name(elem):
     return (elem.attrib['k'] == "addr:street")
 
 
-def audit(osmfile):
-    osm_file = open(osmfile, "r")
-    street_types = set()
-    for event, elem in ET.iterparse(osm_file, events=("start",)):
+###############################################################################################################
 
-        if elem.tag == "node" or elem.tag == "way":
-            for tag in elem.iter("tag"):
-                if is_street_name(tag):
-                    audit_street_type(street_types, tag.attrib['v'])
+def sum_streets_by_type(street_types_count, street_name):
+    street_name = update_abbreviated_name_if_needed(street_name)
 
-    return street_types
+    for acceptable_name in expected_street_types:
+        idx = street_name.strip().lower().find(acceptable_name)
+        if idx >= 0:
+            street_types_count[acceptable_name] += 1
+
+    if street_name.lower().strip().find(' ') < 0:
+        try:
+            street_name_as_number = int(street_name)
+            street_types_count["error"] += 1
+            print 'Street type as a number error: ' + str(street_name_as_number)
+        except:
+            # when street type is not mentioned we default to type "ulica"
+            # this is equivalent to "Street" by country's convention
+            street_types_count["ulica"] += 1
 
 
-def update_name(name, mapping):
+###############################################################################################################
 
-    # YOUR CODE HERE
-
-    for key in mapping:
-        indexes = [match.start() for match in re.finditer(re.escape(key), name)]
-        if len(indexes) > 0:
-            name = name[:indexes[0]]
-            name = name + mapping[key]
+def update_abbreviated_name_if_needed(name):
+    for key in abbreviation_mapping:
+        index = name.find(key)
+        if index >= 0:
+            print name + ' => ' + name.replace(key, abbreviation_mapping[key])
+            name = name.replace(key, abbreviation_mapping[key])
 
     return name
 
 
-def test():
-    st_types = audit(OSMFILE)
-    #assert len(st_types) == 3
-    #pprint.pprint(dict(st_types))
+###############################################################################################################
 
-    #for st_type, ways in st_types.iteritems():
-        #for name in ways:
-            #better_name = update_name(name, mapping)
-            #print name, "=>", better_name
-            # if name == "West Lexington St.":
-            #     assert better_name == "West Lexington Street"
-            # if name == "Baldwin Rd.":
-            #     assert better_name == "Baldwin Road"
+def count_unique_users(filename):
+    users = set()
+    for _, element in ET.iterparse(filename):
+        if element.tag == 'node' or element.tag == 'way' or element.tag == 'relation':
+            uid = element.attrib['uid']
+            # print uid
+            if uid not in users:
+                users.add(uid)
+    print '\nNumber of unique users: ' + str(len(users))
 
+
+###############################################################################################################
 
 def process_map(filename):
     keys = set()
@@ -108,22 +108,19 @@ def process_map(filename):
                 print attribute
 
 
+###############################################################################################################
 
-def count_unique_users(filename):
-    users = set()
+def create_json_from_xml_file(filename):
+    pass
 
-    for _, element in ET.iterparse(filename):
-        if element.tag == 'node' or element.tag == 'way' or element.tag == 'relation':
-            uid = element.attrib['uid']
-            #print uid
-            if uid not in users:
-                users.add(uid)
 
-    print len(users)
-
+###############################################################################################################
 
 if __name__ == '__main__':
-    #test()
-    #process_map(OSMFILE)
-    #count_unique_users(OSMFILE)
-    audit(OSMFILE)
+    # test()
+    # process_map(OSMFILE)
+    pprint.pprint(street_types_from_file(OSMFILE))
+    count_unique_users(OSMFILE)
+    create_json_from_xml_file(OSMFILE)
+    import os
+    print os.getcwd()
