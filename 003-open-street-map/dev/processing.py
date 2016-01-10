1548e691abc5a5d65daabce58703a7cec6f5f7b3
veich <veiczeljko@gmail.com>
2016-01-11 00:59:29 +0100

    DAND - P3 - finished processing.py file

--- 003-open-street-map/dev/processing.py
+++ 003-open-street-map/dev/processing.py
@@ -56,20 +56,30 @@ def is_street_name(elem):
 
 def sum_streets_by_type(street_types_count, street_name):
     street_name = update_abbreviated_name_if_needed(street_name)
+    street_type = return_street_type(street_name)
+    street_types_count[street_type] += 1
+
+    if street_type == 'error':
+        print 'Street name as a number error: ' + str(street_name)
+
+
+###############################################################################################################
+
+def return_street_type(street_name):
+    street_name = update_abbreviated_name_if_needed(street_name)
 
     for acceptable_name in expected_street_types:
         idx = street_name.strip().lower().find(acceptable_name)
         if idx >= 0:
-            street_types_count[acceptable_name] += 1
+            return acceptable_name
 
-    if street_name.lower().strip().find(' ') < 0:
-        if check_valid_street_name(street_name):
-            # when street type is not mentioned we default to type "ulica" by Croatia's convention
-            # "ulica" is equivalent to "Street"
-            street_types_count["ulica"] += 1
-        else:
-            street_types_count["error"] += 1
-            print 'Street name as a number error: ' + str(street_name)
+    # if street_name.lower().strip().find(' ') < 0:
+    if check_valid_street_name(street_name):
+        # when street type is not mentioned we default to type "ulica" by Croatia's convention
+        # "ulica" is equivalent to "Street"
+        return 'ulica'
+    else:
+        return 'error'
 
 
 ###############################################################################################################
@@ -82,6 +92,16 @@ def update_abbreviated_name_if_needed(name):
             name = name.replace(key, abbreviation_mapping[key])
 
     return name
+
+
+###############################################################################################################
+
+def check_valid_street_name(street_name):
+    try:
+        int(street_name)
+        return False
+    except:
+        return True
 
 
 ###############################################################################################################
@@ -150,6 +170,7 @@ def shape_element(element):
                 if check_valid_street_name(tag.attrib['v']):
                     if 'address' not in node:
                         node['address'] = {}
+                        node['address']['type'] = return_street_type(tag.attrib['v'])
                     node['address'][k[5:]] = tag.attrib['v']
 
         for tag in element.iter('nd'):
@@ -160,16 +181,6 @@ def shape_element(element):
         return node
     else:
         return None
-
-
-###############################################################################################################
-
-def check_valid_street_name(street_name):
-    try:
-        int(street_name)
-        return False
-    except:
-        return True
 
 
 ###############################################################################################################
@@ -208,7 +219,7 @@ def check_valid_phone_number(phone_number):
     if phone_number.startswith('+385') and len(phone_number) >= 11:
         return True
     else:
-        print 'Unable to properly format phone number => "' + str(phone_number) + '"\n'
+        print '\nUnable to properly format phone number => "' + str(phone_number) + '"\n'
         return False
 
 ###############################################################################################################
@@ -279,76 +290,73 @@ def save_data_to_file(data):
 
 
 if __name__ == '__main__':
-    # print '\nShow street abbreviation fixes, street name errors and street type count:'
-    # pprint.pprint(street_types_from_file(OSMFILE))
-    # create_list_of_dictionaries_from_xml_file(OSMFILE)
-    #
-    # # dropping the collection to prevent multiple data adding to the MongoDB
-    # mongo_db_drop_collection()
-    #
-    # initialize_mongo_db()
-    db = get_current_mongo_db_instance()
-    # print '\nNumber of documents: ' + str(db.locations.find().count())
-    # print 'Number of nodes: ' + str(db.locations.find({"type": "node"}).count())
-    # print 'Number of ways: ' + str(db.locations.find({"type": "way"}).count()) + '\n'
-    #
-    # mongo_db_make_aggregation_query([{'$match': {'created.user': {'$exists': 1}}},
-    #                                  {'$group': {'_id': '$created.user',
-    #                                              'count': {'$sum': 1}}},
-    #                                  {'$group': {'_id': 'Number of unique users',
-    #                                              'count': {'$sum': 1}}}])
-    # print 'Top 3 contributing users:'
-    # mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
-    #                                              'count': {'$sum': 1}}},
-    #                                  {'$sort': {'count': -1}},
-    #                                  {'$limit': 3}])
-    #
-    # print 'Minimum number of posts per user:'
-    # mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
-    #                                              'count': {'$sum': 1}}},
-    #                                  {'$sort': {'count': 1}},
-    #                                  {'$limit': 1}])
-    #
-    # mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
-    #                                              'count': {'$sum': 1}}},
-    #                                  {'$match': {'count': {'$eq': 1}}},
-    #                                  {'$group': {'_id': 'Number of users having only one post',
-    #                                              'count': {'$sum': 1}}}])
-    #
-    # print 'Top 10 users contribution sum:'
-    # mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
-    #                                              'count': {'$sum': 1}}},
-    #                                  {'$sort': {'count': -1}},
-    #                                  {'$limit': 10},
-    #                                  {'$group': {'_id': 'Sum of top 10 users contributions',
-    #                                              'sum_all': {'$sum': '$count'}}}])
-    #
-    # print 'Top 3 amenities:'
-    # mongo_db_make_aggregation_query([{'$match': {'amenity':{'$exists': 1}}},
-    #                                  {'$group': {'_id': '$amenity',
-    #                                              'count': {'$sum': 1}}},
-    #                                  {'$sort': {'count': -1}},
-    #                                  {'$limit': 3}])
-    #
-    # print 'Most popular cafe chain:'
-    # mongo_db_make_aggregation_query([{'$match': {'amenity': {'$exists': 1}}},
-    #                                  {'$match': {'amenity': 'cafe'}},
-    #                                  {'$group': {'_id': '$name',
-    #                                              'count': {'$sum': 1}}},
-    #                                  {'$sort': {'count': -1}},
-    #                                  {'$limit': 3}])
-    #
-    # print 'Phone numbers sample:'
-    # mongo_db_make_aggregation_query([{'$match': {'phone': {'$exists': 1}}},
-    #                                  {'$project': {'_id': '$phone'}},
-    #                                  {'$limit': 3}])
-    #
-    # mongo_db_make_aggregation_query([{'$match': {'phone': {'$exists': 1}}},
-    #                                  {'$group': {'_id': 'Sum of all phone numbers',
-    #                                              'count': {'$sum': 1}}}])
+    print '\nShow street abbreviation fixes, street name errors and street type count:'
+    pprint.pprint(street_types_from_file(OSMFILE))
+    create_list_of_dictionaries_from_xml_file(OSMFILE)
 
-    mongo_db_make_aggregation_query([{'$match': {'address': {'$exists': 1}}},
+    # dropping the collection to prevent multiple data adding to the MongoDB
+    mongo_db_drop_collection()
+
+    initialize_mongo_db()
+    db = get_current_mongo_db_instance()
+    print '\nNumber of documents: ' + str(db.locations.find().count())
+    print 'Number of nodes: ' + str(db.locations.find({"type": "node"}).count())
+    print 'Number of ways: ' + str(db.locations.find({"type": "way"}).count()) + '\n'
+
+    mongo_db_make_aggregation_query([{'$match': {'created.user': {'$exists': 1}}},
+                                     {'$group': {'_id': '$created.user',
+                                                 'count': {'$sum': 1}}},
+                                     {'$group': {'_id': 'Number of unique users',
+                                                 'count': {'$sum': 1}}}])
+    print 'Top 3 contributing users:'
+    mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
+                                                 'count': {'$sum': 1}}},
+                                     {'$sort': {'count': -1}},
                                      {'$limit': 3}])
+
+    print 'Minimum number of posts per user:'
+    mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
+                                                 'count': {'$sum': 1}}},
+                                     {'$sort': {'count': 1}},
+                                     {'$limit': 1}])
+
+    mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
+                                                 'count': {'$sum': 1}}},
+                                     {'$match': {'count': {'$eq': 1}}},
+                                     {'$group': {'_id': 'Number of users having only one post',
+                                                 'count': {'$sum': 1}}}])
+
+    print 'Top 10 users contribution sum:'
+    mongo_db_make_aggregation_query([{'$group': {'_id': '$created.user',
+                                                 'count': {'$sum': 1}}},
+                                     {'$sort': {'count': -1}},
+                                     {'$limit': 10},
+                                     {'$group': {'_id': 'Sum of top 10 users contributions',
+                                                 'sum_all': {'$sum': '$count'}}}])
+
+    print 'Top 3 amenities:'
+    mongo_db_make_aggregation_query([{'$match': {'amenity':{'$exists': 1}}},
+                                     {'$group': {'_id': '$amenity',
+                                                 'count': {'$sum': 1}}},
+                                     {'$sort': {'count': -1}},
+                                     {'$limit': 3}])
+
+    print 'Most popular cafe chain:'
+    mongo_db_make_aggregation_query([{'$match': {'amenity': {'$exists': 1}}},
+                                     {'$match': {'amenity': 'cafe'}},
+                                     {'$group': {'_id': '$name',
+                                                 'count': {'$sum': 1}}},
+                                     {'$sort': {'count': -1}},
+                                     {'$limit': 3}])
+
+    print 'Phone numbers sample:'
+    mongo_db_make_aggregation_query([{'$match': {'phone': {'$exists': 1}}},
+                                     {'$project': {'_id': '$phone'}},
+                                     {'$limit': 3}])
+
+    mongo_db_make_aggregation_query([{'$match': {'phone': {'$exists': 1}}},
+                                     {'$group': {'_id': 'Sum of all phone numbers',
+                                                 'count': {'$sum': 1}}}])
 
 
 ###############################################################################################################
